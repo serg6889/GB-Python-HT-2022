6ecee8ed9f5f706e65df017390a11a5a647e508b
Sergey68 <sesergey@yandex.ru>
2022-09-05 23:03:00 +1100

    File is updated with code

--- home task4.py
+++ home task4.py
@@ -1,3 +1,12 @@
 #Напишите программу, которая по заданному номеру четверти 
 # показывает диапазон возможных координат точек в этой четверти (x и y).
 
+quaternum = int(input('Input Quater N- '))
+if quaternum == 1:
+    print('X => 0 and Y => 0')
+elif quaternum == 2:
+    print('X => 0 and Y =< 0')
+elif quaternum == 3:
+    print('X =< 0 and Y => 0')
+elif quaternum == 4:
+    print('X =< 0 and Y =< 0')
