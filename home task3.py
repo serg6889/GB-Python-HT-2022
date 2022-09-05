a38294cd75fbafae827cb0f10bf8e3e70e12af21
Sergey68 <sesergey@yandex.ru>
2022-09-05 22:19:55 +1100

    File is updated with revised code

--- home task3.py
+++ home task3.py
@@ -2,4 +2,13 @@
 # причём X ≠ 0 и Y ≠ 0 и выдаёт номер четверти плоскости, 
 # в которой находится эта точка (или на какой оси она находится).
 
-x =
+x = int(input('Input X = '))
+y = int(input('Input Y = '))
+if x > 0 and y > 0:
+    print('Quater N-1')
+elif x > 0 and y < 0:
+    print('Quater N-2')
+elif x < 0 and y < 0:
+    print('Quater N-4')
+elif x < 0 and y > 0:
+    print('Quater N-3')
