3496478825747889756d23cddd832b14bd5c1d99
Sergey68 <sesergey@yandex.ru>
2022-09-12 00:42:10 +1100

    File is updated with code

---
+++ home task26.py
@@ -0,0 +1,11 @@
+#Напишите программу, в которой пользователь будет задавать две строки,
+#а программа - определять количество вхождений одной строки в другой.
+str1 = str(input())
+str2 = str(input())
+
+numcross = 0
+
+for i in range(len(str1) - len(str2)+1):
+    if str1[i: i+ len(str2)] == str2:
+        numcross += i
+print(numcross)    
