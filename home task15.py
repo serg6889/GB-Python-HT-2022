aa3c5424ccec0e2a37c567a9e78d2813dca8cb51
Sergey68 <sesergey@yandex.ru>
2022-09-11 16:19:19 +1100

    Initial commit

--- home task15.py
+++ home task15.py
@@ -2,6 +2,6 @@
 # и находит расстояние между ними в 2D пространстве.
 x1,y1=map(int,input('X1,Y1:').split())
 x2,y2=map(int,input('X2,Y2:').split())
-d = ((x1-x2)**2+(y1-y2)**2)**0.5
+d = (round(((x1-x2)**2+(y1-y2)**2)**0.5),2)
 print('Distance =', d )
 
---
+++ lec21.py
@@ -0,0 +1,16 @@
+# n = int(input('Input number N= '))
+
+# a = [1]
+# for i in range (1, n):
+#     a.append(a[-1]* (-3))
+# print(a)
+
+# n = int(input('Input number '))
+# num = 1
+# for i in range(n):
+#     print(num)
+#     num *= -3
+
+for i in range (int(input("Input number= "))):
+    print((-3) ** i)
+    
