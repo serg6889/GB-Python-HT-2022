1a95777e8152e3ddbd419030e5f6543514255c1b
Sergey68 <sesergey@yandex.ru>
2022-09-11 16:46:32 +1100

    File is updated with code

--- home task21.py
+++ home task21.py
@@ -1,1 +1,22 @@
 #Напишите программу, которая принимает на вход вещественное число и показывает сумму его цифр.
+
+# num = int(input('Input number  '))
+# sumdigit = 0
+
+# while num > 0:
+#     digit = num % 10
+#     sumdigit = sumdigit + digit 
+#     num = num // 10
+
+# print (sumdigit)    
+
+num = input('Input number  ')
+sum = 0
+
+for digit in num:
+    sum += int(digit)
+
+print (sum)
+
+
+
