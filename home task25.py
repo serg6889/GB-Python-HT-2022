8b6ff8dd1d3e34593d8eef92dac99b2297e31b75
Sergey68 <sesergey@yandex.ru>
2022-09-14 21:28:31 +1100

    Added code developed initially  and commented

--- home task25.py
+++ home task25.py
@@ -1,4 +1,11 @@
 #Реализуйте алгоритм перемешивания списка.
+# num = int(input())
+# a = [ ]
+# for i in range (num+1):
+#    a.append(i+1)
+# print(a)
+#   b = a.copy(-i)
+# print(b)
 
 import random
 list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
