9bcadf8cf2e7b5f44548b1cd5834d652a48aeba9
Sergey68 <sesergey@yandex.ru>
2022-09-14 19:12:35 +1100

    File is updated with revised code

--- home task25.py
+++ home task25.py
@@ -1,1 +1,12 @@
 #Реализуйте алгоритм перемешивания списка.
+
+import random
+list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+last_position = len(list) - 1
+while last_position > 1:
+    random_index = random.randint(0, last_position)
+    list[last_position], list[random_index] = list[random_index], list[last_position]
+    last_position -= 1
+   
+print(list)
