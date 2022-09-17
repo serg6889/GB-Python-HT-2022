358fc1c62264879a0dc7dd0f607f93d8b65f8cff
Sergey68 <sesergey@yandex.ru>
2022-09-17 15:28:04 +1100

    Initial commit

---
+++ home task34.py
@@ -0,0 +1,22 @@
+#Напишите программу, которая будет преобразовывать десятичное число в двоичное.
+#Пример:
+#45 -> 101101
+
+# n = int(input())
+# bin = ''
+# while n > 0:
+#     bin = str(n % 2) + b
+#     n = n // 2
+# print(bin)   
+ 
+
+bin = ''
+def bin(n):
+    b = ''
+while n > 0:
+        b = str(n % 2) + b
+        n = n // 2
+    return b
+ 
+n = int(input())
+print(bin(n))
