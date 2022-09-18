786b426dc18c2d521f5a4f51b96392d5b2ebb2d0
Sergey68 <sesergey@yandex.ru>
2022-09-18 12:31:28 +1100

    File is updated with code

--- home task35.py
+++ home task35.py
@@ -2,5 +2,17 @@
 #Пример:
 # для k = 8 список будет выглядеть так: [-21 ,13, -8, 5, −3, 2, −1, 1, 0, 1, 1, 2, 3, 5, 8, 13, 21]  
 
-def negfib(n):
-    
+def fib(n):
+    if n > -1:
+        if n ==0:
+            return 0    
+        if n==1 or n==2:
+            return 1
+        else:
+            return fib(n+1) + fib(n+2)
+
+    if n < -1:
+        return ((-1)**(n+1))*fib(-n)
+
+n = int(input('Input number = \n'))
+print(fib(n))
