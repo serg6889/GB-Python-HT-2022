f7459a277318560f21ddd23020ff845ccdae6924
Sergey68 <sesergey@yandex.ru>
2022-09-21 16:00:43 +1100

    File is updated with code

--- home task43.py
+++ home task43.py
@@ -1,11 +1,25 @@
 #Задайте последовательность чисел. Напишите программу, 
 # которая выведет список неповторяющихся элементов исходной последовательности.
 
+# def nonrep_l(n):
+#     nonrep_l = []
+#     for i in range (len(n)):
+#         a = int(n[i])
+#         if a != int(n[i+1]):
+#             nonrep_l.append(a)
+#             i+=1
+#     return nonrep_l
+
+# n = list(input("Input numbers \n"). split( ))   
+# print(n)
+
+# print(nonrep_l(n))
+
 def nonrep_l(n):
     nonrep_l = []
-    for i in range (len(n+1)):
-        a = int(n[i])
-        if a != int(n[i+1]):
+    for i in range (len(n)):
+        a = n[i]
+        if a not in nonrep_l:
             nonrep_l.append(a)
             i+=1
     return nonrep_l
