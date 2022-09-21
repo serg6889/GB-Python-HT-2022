a7a4cf7a7095f0e82e22c518b8fd662e7147b664
Sergey68 <sesergey@yandex.ru>
2022-09-21 15:01:50 +1100

    sFile is  updated with code  Please enter the commit message for your changes. Lines starting

--- home task42.py
+++ home task42.py
@@ -1,1 +1,26 @@
 # Заадайте натуральное число N. Напишите программу, которая составит список простых множителей числа N.
+
+
+# def divider_num(n):
+    
+#     for i in range(2, n):
+#         , while i < n**0.5]
+
+    
+#         return i
+
+def num_multipl(n):
+   i = 2
+   num_multipl = []
+   while i * i <= n:
+       while n % i == 0:
+           num_multipl.append(i)
+           n = n / i
+       i = i + 1
+   if n > 1:
+       num_multipl.append(n)
+   return num_multipl
+
+n = int(input('Input number = \n'))  
+print(num_multipl(n))
+ 
