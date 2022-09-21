0857b046c118cd005c19970f975ebbc7bffdab5b
Sergey68 <sesergey@yandex.ru>
2022-09-21 13:27:47 +1100

    File is updated with code

--- home task41.py
+++ home task41.py
@@ -1,2 +1,17 @@
 #Вычислить число π c заданной точностью d
 #*Пример:* - при $d = 0.001, π = 3.141.$    $10^{-1} ≤ d ≤10^{-10}$
+
+d = int(input('Input requested precision of Pi = \n'))
+k = 1
+
+s = 0
+ 
+for i in range(d):
+    if i % 2 == 0:
+        s += 4/k
+    else:
+        s -= 4/k
+ 
+    k += 2
+     
+print(s)
